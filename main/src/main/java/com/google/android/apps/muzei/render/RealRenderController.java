d6ac0781c15b762357ecc15dcda264851e6d0028
Ian Lake <ilake@google.com>
2016-11-11 13:39:27 -0800

    Parameterize what URI is loaded by RealRenderController

--- main/src/main/java/com/google/android/apps/muzei/render/RealRenderController.java
+++ main/src/main/java/com/google/android/apps/muzei/render/RealRenderController.java
@@ -61,7 +61,7 @@ public class RealRenderController extends RenderController {
         try {
             // Check if there's rotation
             int rotation = 0;
-            try (InputStream in = mContext.getContentResolver().openInputStream(MuzeiContract.Artwork.CONTENT_URI)) {
+            try (InputStream in = mContext.getContentResolver().openInputStream(mImageUri)) {
                 if (in == null) {
                     return null;
                 }
